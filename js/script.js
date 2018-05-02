6d8e46fb3f200e870d1b8683292a00bd520ca68d
Jianlin Feng <jfeng0083@yahoo.com>
2018-05-02 16:40:12 -0400

    Connect to New York Times Article Search API.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-var apiKey = "AIzaSyDEbGSkMfRjHlj7M4s6QTxN6vi7IMSfLYA";
+var googleApiKey = "AIzaSyDEbGSkMfRjHlj7M4s6QTxN6vi7IMSfLYA";
+var nytApiKey = "ee7d255929f745f68e7ed0ded40ab29e";
 var googleStreetViewURL = "http://maps.googleapis.com/maps/api/streetview?size=600x300&location=";
 function loadData() {
 
@@ -9,28 +10,49 @@ function loadData() {
     var $greeting = $('#greeting');
 
     // clear out old data before new request
-    $wikiElem.text("");
+//    $wikiElem.text("");
     $nytElem.text("");
 
     // load streetview
-
     // YOUR CODE GOES HERE!
 	var streetStr = $("#street").val();
 	var cityStr = $("#city").val();
 	var address = "";
 	
-	if ( streetStr && cityStr ){ address = streetStr + ', ' + cityStr; }
+	if ( streetStr && cityStr ){ 
+		address = streetStr + ', ' + cityStr; 
+	}
 	else if ( streetStr ) { address = streetStr; }
 	else if ( cityStr ) { address = cityStr; }
-	
-	if ( address ) {
+	console.log(address);
+	if ( address.length > 0 ) {
 		$greeting.text("These are the info related to " + address + ".");
 		$body.append('<img class = "bgimg" src="' + googleStreetViewURL + address + '">');
 	}
 	else {
 		$greeting.text("No address found. Please enter a new address.")
 	}
-    return false;
+	
+	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+	url += '?' + $.param({
+		'api-key': nytApiKey,
+		'q': address
+		});
+	
+	$.getJSON(url, function(result){
+		var docs = result.response.docs;
+		console.log(docs);
+		var nytArticles = '<ul id="nytimes-articles" class="articles-list">';
+ 		for( var i = 0; i < docs.length; i++ ){
+			if ( docs[i].document_type != "topic" ){
+				nytArticles += `<li class="article"><a href="${docs[i].web_url}">${docs[i].headline.main}</a><p>"${docs[i].snippet}"</p></li>`;
+			}
+		} 
+		nytArticles += '</ul>';
+		$nytElem.append(nytArticles);
+	});
+	
+	return false;
 };
 
 $('#form-container').submit(loadData);
