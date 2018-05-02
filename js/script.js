391eb0c01f0ac7630f3572ecddfab1ee791cb8e9
Jianlin Feng <jfeng0083@yahoo.com>
2018-05-02 14:40:16 -0400

    Connect to Google Street View API.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-
+var apiKey = "AIzaSyDEbGSkMfRjHlj7M4s6QTxN6vi7IMSfLYA";
+var googleStreetViewURL = "http://maps.googleapis.com/maps/api/streetview?size=600x300&location=";
 function loadData() {
 
     var $body = $('body');
@@ -14,7 +15,21 @@ function loadData() {
     // load streetview
 
     // YOUR CODE GOES HERE!
-
+	var streetStr = $("#street").val();
+	var cityStr = $("#city").val();
+	var address = "";
+	
+	if ( streetStr && cityStr ){ address = streetStr + ', ' + cityStr; }
+	else if ( streetStr ) { address = streetStr; }
+	else if ( cityStr ) { address = cityStr; }
+	
+	if ( address ) {
+		$greeting.text("These are the info related to " + address + ".");
+		$body.append('<img class = "bgimg" src="' + googleStreetViewURL + address + '">');
+	}
+	else {
+		$greeting.text("No address found. Please enter a new address.")
+	}
     return false;
 };
 
